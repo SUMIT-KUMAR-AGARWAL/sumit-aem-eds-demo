325c57acad1c3c699987a7650639f0de99061afd
SUMIT-KUMAR-AGARWAL <skagarwal7757@gmail.com>
2024-04-22 13:32:40 +0530

    Updated Tab CSS & JS

--- blocks/tab/tab.js
+++ blocks/tab/tab.js
@@ -8,17 +8,17 @@ function hasWrapper(el) {
 export default async function decorate(block) {
   // build tablist
   const tablist = document.createElement('div');
-  tablist.className = 'tabs-list';
+  tablist.className = 'tab-list';
   tablist.setAttribute('role', 'tablist');
 
-  // decorate tabs and tabpanels
-  const tabs = [...block.children].map((child) => child.firstElementChild);
-  tabs.forEach((tab, i) => {
+  // decorate tab and tabpanels
+  const tab = [...block.children].map((child) => child.firstElementChild);
+  tab.forEach((tab, i) => {
     const id = toClassName(tab.textContent);
 
     // decorate tabpanel
     const tabpanel = block.children[i];
-    tabpanel.className = 'tabs-panel';
+    tabpanel.className = 'tab-panel';
     tabpanel.id = `tabpanel-${id}`;
     tabpanel.setAttribute('aria-hidden', !!i);
     tabpanel.setAttribute('aria-labelledby', `tab-${id}`);
@@ -29,7 +29,7 @@ export default async function decorate(block) {
 
     // build tab button
     const button = document.createElement('button');
-    button.className = 'tabs-tab';
+    button.className = 'tab-button';
     button.id = `tab-${id}`;
     button.innerHTML = tab.innerHTML;
     button.setAttribute('aria-controls', `tabpanel-${id}`);
